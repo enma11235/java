44c508c54770399410c46ba134bfcd811431f63b
enma <enmaundostres@gmail.com>
2024-02-24 17:35:27 -0300

    creando una clase

--- helloWorld.java
+++ helloWorld.java
@@ -1,17 +1,21 @@
-public class helloWorld {
-    public static void signo(int x) {
-        if (x > 0) {
-            System.out.print("positivo");
-        } else if(x == 0) {
-            System.out.print("cero");
-        } else {
-            System.out.print("negativo");
-        }
+class Persona {
+    String nombre;
+    int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
     }
 
+    public void saludar() {
+        System.out.println("Hola, mi nombre es " + nombre + "y tengo " + edad + " anios");
+    }
+
+}
+
+public class helloWorld {
     public static void main(String[] args) {
-        String par1 = args[1];
-        int x = Integer.parseInt(par1);
-        System.out.println(x);
+        Persona persona = new Persona("Juan", 25);
+        persona.saludar();
     }
 }

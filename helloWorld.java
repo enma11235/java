876969d855f352eba2e691785d3758100cd4df91
enma <enmaundostres@gmail.com>
2024-02-24 17:02:40 -0300

    creando funcion

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,11 @@
 public class helloWorld {
+    public static int sumar(int num1, int num2) {
+        int res = num1 + num2;
+        return res;
+    }
+
     public static void main(String[] args) {
-        boolean x = true;
-        System.out.println(x);
+        int resultadoSuma = sumar(2, 2);
+        System.out.println("La suma es: " + resultadoSuma);
     }
 }
